5380483988fc5f222ae5cdaf2c5635af8cf98838
fish-and-bear <jessegarfieldscats@gmail.com>
2023-05-22 02:09:15 +0800

    REFACTOR: Optimize code

--- main.c
+++ main.c
@@ -1,103 +1,98 @@
 /************************************************************************************************************************************
-*  CMSC 128 Problem Set: Money Dispenser
-*
-*  This program dispenses money from a machine. 
-*  The program first asks the user to input the number of PHP 100 bills, PHP 50 bills, PHP 20 bills, PHP 5 coins, and PHP 1 coins.
-*  The program, then, asks the user to input the money they want to dispense.
-*  If possible, the program will dispense the amount of money requested using the minimum number of bills and coins.
-*  The program will stop when the user inputs 0 or when there are no more bills or coins left.
-*
-*  Author: Angelica Anne A. Naguio
-*  Date: 2023-05-21
-* 
-*************************************************************************************************************************************/
+ *  CMSC 128 Problem Set: Money Dispenser
+ *
+ *  This program dispenses money from a machine.
+ *  The program first asks the user to input the number of PHP 100 bills, PHP 50 bills, PHP 20 bills, PHP 5 coins, and PHP 1 coins.
+ *  The program, then, asks the user to input the money they want to dispense.
+ *  If possible, the program will dispense the amount of money requested using the minimum number of bills and coins.
+ *  The program will stop when the user inputs 0 or when there are no more bills or coins left.
+ *
+ *  Author: Angelica Anne A. Naguio
+ *  Date: 2023-05-21
+ *
+ *************************************************************************************************************************************/
 
 #include <stdio.h>
 
-int main() {
-    int remainingHundredBills, remainingFiftyBills, remainingTwentyBills, remainingFiveCoins, remainingOneCoins;
-    int dispensedHundredBills = 0, dispensedFiftyBills = 0, dispensedTwentyBills = 0, dispensedFiveCoins = 0, dispensedOneCoins = 0;
-    int amount, remainingAmount, dispensedAmount;
+int main()
+{
+    // variables
+    int bills[5] = {100, 50, 20, 5, 1};
+    int remainingBills[5] = {0}, dispensedBills[5] = {0};
+    int numBills = sizeof(bills) / sizeof(bills[0]);
+    int amount, dispensedAmount;
 
     // get user input
-    printf("Enter the number of PHP 100 bills: ");
-    scanf("%d", &remainingHundredBills);
+    for (int i = 0; i < numBills; i++)
+    {
+        printf("Enter the number of PHP%d bills: ", bills[i]);
+        scanf("%d", &remainingBills[i]);
+    }
 
-    printf("Enter the number of PHP 50 bills: ");
-    scanf("%d", &remainingFiftyBills);
-
-    printf("Enter the number of PHP 20 bills: ");
-    scanf("%d", &remainingTwentyBills);
-
-    printf("Enter the number of PHP 5 coins: ");
-    scanf("%d", &remainingFiveCoins);
-
-    printf("Enter the number of PHP 1 coins: ");
-    scanf("%d", &remainingOneCoins);
-
-    while(1) {
+    // main loop
+    while (1)
+    {
+        // Get input from the user
         printf("Dispensing amount: ");
         scanf("%d", &amount);
-        
+
         // if the user inputs 0, stop the program
-        if (amount == 0) {
+        if (amount == 0)
+        {
             break;
         }
 
         // reset dispensed amount every loop
         dispensedAmount = 0;
-        dispensedHundredBills = 0;
-        dispensedFiftyBills = 0;
-        dispensedTwentyBills = 0; 
-        dispensedFiveCoins = 0;
-        dispensedOneCoins = 0;
-        while (amount > 0) {
-            if (remainingHundredBills > 0 && amount >= 100) {
-                dispensedAmount += 100;
-                amount -= 100;
-                remainingHundredBills--;
-                dispensedHundredBills++;
-            } else if (remainingFiftyBills > 0 && amount >= 50) {
-                dispensedAmount += 50;
-                amount -= 50;
-                remainingFiftyBills--;
-                dispensedFiftyBills++;
-            } else if (remainingTwentyBills > 0 && amount >= 20) {
-                dispensedAmount += 20;
-                amount -= 20;
-                remainingTwentyBills--;
-                dispensedTwentyBills++;
-            } else if (remainingFiveCoins > 0 && amount >= 5) {
-                dispensedAmount += 5;
-                amount -= 5;
-                remainingFiveCoins--;
-                dispensedFiveCoins++;
-            } else if (remainingOneCoins > 0 && amount >= 1) {
-                dispensedAmount += 1;
-                amount -= 1;
-                remainingOneCoins--;
-                dispensedOneCoins++;
-            } else { // not enough money
-                printf("Not enough money in the machine\n");
-                dispensedAmount = 0;
-                remainingHundredBills += dispensedHundredBills;
-                remainingFiftyBills += dispensedFiftyBills;
-                remainingTwentyBills += dispensedTwentyBills;
-                remainingFiveCoins += dispensedFiveCoins;
-                remainingOneCoins += dispensedFiveCoins;
-                break;
+        for (int i = 0; i < numBills; i++)
+        {
+            dispensedBills[i] = 0;
+        }
+
+        // calculate minimum possible dispensed bills
+        for (int i = 0; i < numBills; i++)
+        {
+            while (remainingBills[i] > 0 && amount >= bills[i])
+            {
+                dispensedAmount += bills[i];
+                amount -= bills[i];
+                remainingBills[i]--;
+                dispensedBills[i]++;
             }
         }
 
-        // print the dispensed amount
-        if (dispensedAmount > 0) {
-            printf("Amount: %d P100, %d P50, %d P20, %d P5, %d P1\n", 
-                    dispensedHundredBills, dispensedFiftyBills, dispensedTwentyBills, dispensedFiveCoins, dispensedFiveCoins
-            );
+        // if money in the machine is not enough to dispense amount
+        if (amount > 0)
+        {
+            printf("Not enough money in the machine\n");
+            dispensedAmount = 0;
+            for (int i = 0; i < numBills; i++)
+            {
+                remainingBills[i] += dispensedBills[i]; // reset remaining bills to value before calculation
+            }
+            // if dispensing the amount is possible with the bills and coins available
         }
-
-        // update the remaining amount
-        remainingAmount = amount;
+        else
+        {
+            // print the dispensed amount
+            printf("Amount: ");
+            for (int i = 0; i < numBills; i++)
+            {
+                if (dispensedBills[i] > 0)
+                {
+                    printf("%d P%d", dispensedBills[i], bills[i]);
+                    for (int j = i + 1; j < numBills; j++)
+                    {
+                        if (dispensedBills[j] > 0)
+                        {
+                            printf(", ");
+                            break;
+                        }
+                    }
+                }
+            }
+            printf("\n");
+        }
     }
     printf("End\n");
     return 0;
